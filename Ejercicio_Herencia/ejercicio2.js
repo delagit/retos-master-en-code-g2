6005c49d81d9aa0e627ef13769df8309a5b0295a
Johnny A. López <johnlopez@hey.com>
2020-09-21 13:54:38 -0600

    Segundo ejercicio completado

--- Ejercicio_Herencia/ejercicio2.js
+++ Ejercicio_Herencia/ejercicio2.js
@@ -6,7 +6,60 @@
     porcentajeAlcohol
 Crear los getters y setters correspondientes. */
 
+class Bebida {
+    constructor(mL){
+        this.mL = mL;
+    }
+}
+
+class Cerveza extends Bebida {
+    constructor(mL, porcentajeAlcohol){
+        super(mL);
+        this.porcentajeAlcohol = porcentajeAlcohol;
+    }
+    get verPorcentajeAlcohol(){
+        return `El porcentaje de alcohol de esta bebida es ${this.porcentajeAlcohol}%`;
+    }
+    setcambiarPorcentajeAlcohol(nuevoPorcentajeAlcohol){
+        this.porcentajeAlcohol = nuevoPorcentajeAlcohol;
+        return `El porcentaje de alcohol de esta bebida es ${this.porcentajeAlcohol}%`;
+    }
+    get verML(){
+        return `La capacidad del envase de la bebida es de ${this.mL}mL`;
+    }
+    setnuevoML(nuevoML){
+        this.mL = nuevoML;
+        return `La capacidad del envase de la bebida es de ${this.mL}mL`;
+    }
+}
+
+
+const angelCaido = new Cerveza(450,9);
+console.log(angelCaido);
+console.log("=============================================================");
 
 
 
+class Refresco extends Bebida{
+    constructor(mL, azucar){
+        super(mL);
+        this.azucar = azucar;
+    }
+    get verML(){
+        return `La capacidad del envase de la bebida es de ${this.mL}mL`;
+    }
+    setcambiarML(nuevoML){
+        this.mL = nuevoML;
+        return `La capacidad del envase de la bebida es de ${this.mL}mL`;
+    }
+    get verAzucar(){
+        return `La cantidad de azucar en esta bebida es de ${this.azucar} gramos`;
+    }
+    setcambiarAzucar(nuevoAzucar){
+        this.azucar = nuevoAzucar;
+        return `La cantidad de azucar en esta bebida es de ${this.azucar} gramos`;
+    }
+}
 
+const felix = new Refresco(355, 14);
+console.log(felix);
